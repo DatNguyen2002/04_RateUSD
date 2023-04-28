fb443d13603e69b53ae3fcdc9c0a3a83e46dc2da
DatNguyen2002 <90488040+DatNguyen2002@users.noreply.github.com>
2023-04-27 22:27:45 -0700

    Chuyen Doi Ti Gia
    USD => VND

---
+++ RateUSD/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace RateUSD
+{
+    class Program
+    {
+        static void Main(string[] args){
+            const int RateUSD = 23000;
+            
+            Console.Write("Nhap So USD ma ban muon chuyen: ");
+            float USD =  float.Parse(Console.ReadLine());
+
+            Console.WriteLine(USD + " USD = " + (USD * RateUSD) + " VND ");
+
+        }
+    }
+}
